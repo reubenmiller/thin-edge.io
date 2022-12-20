8e4e2aee804bdbcd6b213678dd61d36ce5484084
Reuben Miller <reuben.miller@softwareag.com>
2022-12-20 12:58:00 +0000

    fixup! feat(tests): add container device testing

--- tests/RobotFramework/bin/setup.sh
+++ tests/RobotFramework/bin/setup.sh
@@ -17,6 +17,7 @@ python3 -m venv env
 # shellcheck source=/dev/null
 source env/bin/activate
 
+pip3 install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements-dev.txt
 
@@ -47,14 +48,20 @@ show_dotenv_help () {
 }
 
 if [ ! -f "$DOTENV_FILE" ]; then
+    echo
+    echo
     echo "Creating the .env file from the template"
     cp "$DOTENV_TEMPLATE" "$DOTENV_FILE"
     show_dotenv_help
 elif ! grep "# Testing" "$DOTENV_FILE" >/dev/null; then
+    echo
+    echo
     echo "Adding required Testing variables to your existing .env file"
     cat "$DOTENV_TEMPLATE" >> "$DOTENV_FILE"
     show_dotenv_help
 else
+    echo
+    echo
     echo "Your .env file already contains the '# Testing' section"
     echo "If test tests are still not working then check for any newly added settings in the template .env file"
     echo
@@ -79,7 +86,8 @@ fi
 #
 # Build docker images (required for container devices)
 #
-echo "Building containerized devices images"
-invoke build
+if ! invoke build >/dev/null 2>&1; then
+    echo "Failed to build container image. Please try running 'invoke build' manually to debug the output"
+fi
 
 popd >/dev/null || exit 1
